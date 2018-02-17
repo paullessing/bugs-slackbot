007a5db95a13b95968eca456425728f57ff0fdf3
paullessing <paul@paullessing.com>
2018-02-17 23:30:09 +0000

    Remove accidental commit

--- config/config.sample.ts
+++ config/config.sample.ts
@@ -1,7 +1,7 @@
 export const config = {
   slackUrl: "/services/XXXXXXXXX/YYYYYYYYY/zzzzzzzzzzzzzzzzzzzzzzzz",
-  username: "Demo Environments",
-  channel: "#demo-envs",
+  username: "Bug Watcher",
+  channel: "#bugs",
   jiraUser: "user@email.com",
   jiraToken: "token from https://id.atlassian.com/manage/api-tokens",
   jiraServer: "servername.atlassian.net"
--- config/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  slackUrl: "/services/T02V5NQ8B/B9ALJ8NJX/x1UPam7sbRmUO1ym2zGP14Vu",
-  username: "Bug Tracker",
-  channel: "#test-hooks",
-  jiraUser: "paul@citypantry.com",
-  jiraToken: "37ZdFnxureVaxWdeqkXB9905",
-  jiraServer: "https://citypantry.atlassian.net"
-};
